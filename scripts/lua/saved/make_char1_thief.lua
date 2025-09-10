84628e0e7ec699feba1cacecc8ff70432868ab8e
Chris Shinnimin <cshinnimin@gmail.com>
2025-09-10 14:08:55 -0700

    fine tuning instructions, restructure folders

--- scripts/lua/saved/make_char1_thief.lua
+++ scripts/lua/saved/make_char1_thief.lua
@@ -1,6 +1,6 @@
 -- Define the address and the new value
-local address = 0x006100 -- Example address (can be in decimal or hexadecimal)
-local newValue = 0x01   -- Example value to write
+local address = 0x006100 -- Example address, in hexadecimal
+local newValue = 0x01   -- Example value to write, in hexadecimal
 
 -- Write the new value to the specified address
 memory.writebyte(address, newValue)
