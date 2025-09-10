c0d0f3b10d6961bac04a97a0efd914fa3bc15671
Chris Shinnimin <cshinnimin@gmail.com>
2025-09-10 16:29:49 -0700

    commit vite bot-app initial container

---
+++ bot-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import MainContainer from './components/containers/MainContainer';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <MainContainer />
+      </div>
+    </>
+  )
+}
+
+export default App
