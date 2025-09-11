9957c3f555661cb1cfb1dfa0efa4c672e3c8173e
Chris Shinnimin <cshinnimin@gmail.com>
2025-09-10 18:31:28 -0700

    update adventurer avatar

--- bot-app/src/components/presentational/ChatMessage.tsx
+++ bot-app/src/components/presentational/ChatMessage.tsx
@@ -10,20 +10,21 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ persona, message }) => {
   const chatClass = persona === 'User' ? 'chat chat-start' : 'chat chat-end';
   const headerText = persona === 'Bot' ? 'FFBot' : 'Adventurer';
 
+  const avatarSrc = persona === 'User' ? '/images/user-avatar.png' : '/images/ffbot-avatar.png';
   return (
     <div className={chatClass}>
-        <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-                <img
-                    alt="Final Fantasy Bot Avatar"
-                    src="/images/ffbot-avatar.png"
-                />
-            </div>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt={persona === 'User' ? 'Adventurer Avatar' : 'Final Fantasy Bot Avatar'}
+            src={avatarSrc}
+          />
         </div>
-        <div className="chat-header">
-            {headerText}
-        </div>
-        <div className="chat-bubble">{message}</div>
+      </div>
+      <div className="chat-header">
+        {headerText}
+      </div>
+      <div className="chat-bubble">{message}</div>
     </div>
   );
 };
