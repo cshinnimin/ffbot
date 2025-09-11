dd21c0cb85f5c0fb200449c7d41c93d344bceb6c
Chris Shinnimin <cshinnimin@gmail.com>
2025-09-11 13:09:05 -0700

    update comments

--- bot-app/src/components/containers/ChatContainer.tsx
+++ bot-app/src/components/containers/ChatContainer.tsx
@@ -5,8 +5,10 @@ import ChatLayout from '../presentational/ChatLayout';
 import { useAppMessages } from '../../context/AppMessagesContext';
 
 function ChatContainer() {
+  // first set up what we need from context/state
   const { appMessages, addAppMessage } = useAppMessages();
 
+  // now define any behaviours we need
   const handleSend = (message: string) => {
     // messages from ChatInput are always from the User
     addAppMessage('User', message);
